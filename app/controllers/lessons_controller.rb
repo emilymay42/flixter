5f1bf64a03a9a9a8da10fd101f3dcfd440db5a61
Emily Nelson <emilymay42@gmail.com>
2016-02-04 23:25:55 +0000

    Restrict lessons to enrolled students

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_enrolled_for_current_course
+
 	def show
 	end
 
@@ -7,4 +10,11 @@ class LessonsController < ApplicationController
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
 	end
+
+	def require_enrolled_for_current_course
+		if !current_user.enrolled_in?(current_lesson.section.course)
+			redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled to view lessons'
+		end
+	end
+
 end
